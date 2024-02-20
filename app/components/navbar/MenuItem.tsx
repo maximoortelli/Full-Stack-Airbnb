0a6d670a620f171ac0cd84614caf0fd27ce25195
maximoortelli <maximoortelli@hotmail.com>
2024-02-20 11:13:39 -0300

    Added all files
    Intalled Toaster
    Added Modal.tsx file.
    Added RegisterModal.tsx file.
    Added Inputtsx file.
    Added UseRegisterModal.tsx file

--- app/components/navbar/MenuItem.tsx
+++ app/components/navbar/MenuItem.tsx
@@ -1,16 +1,17 @@
-"use client";
+import React from "react"; // Asegúrate de importar React
 
 interface MenuItemProps {
   onClick: () => void;
   label: string;
 }
 
-const MenuItem: React.FC<MenuItemProps> = ({ onclick, label }) => {
+const MenuItem: React.FC<MenuItemProps> = ({ onClick, label }) => {
   return (
-  <div onClick={onclick} className="px-4 py-3 hover:bg-neutral-100 transition font-semibold">
-    {label}
-   </div>
- );
+    <div onClick={onClick} className="px-4 py-3 hover:bg-neutral-100 transition font-semibold">
+      {label}
+    </div>
+  );
 };
 
 export default MenuItem;
+
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,14 @@
+/*Acordate la ruta del proyecto esta mal
+antes de correr el proyecto entra a "cd full-stack-airbnb" */
 import type { Metadata } from "next";
-import { Nunito} from "next/font/google";
+import { Nunito } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
 import ClientOnly from "./components/ClientOnly";
 import Modal from "./components/modals/Modal";
+import RegisterModal from "./components/modals/RegisterModal";
+import ToasterProvider from "./components/providers/ToasterProvider";
+
 
 const font = Nunito({ subsets: ["latin"] });
 
@@ -21,8 +26,10 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
-           <Modal />
-           <Navbar />
+          <ToasterProvider />
+          {/*<Modal actionLabel="Submit" title="Hello World" isOpen={true} /> */}
+          <RegisterModal />
+          <Navbar />
         </ClientOnly>
         {children}
       </body>
